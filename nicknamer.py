83e7fcb89f9a65b2b9004d4b17e5d22360085a64
Libruhh <laif11c@gmail.com>
2022-05-20 16:17:48 -0500

    added adjustments

--- nicknamer.py
+++ nicknamer.py
@@ -17,6 +17,9 @@ OUTPUT_CHANNEL = int(os.getenv('OUTPUT_CHANNEL'))
 client = discord.Client()
 co = cohere.Client(COHERE_KEY)
 
+introMessage = None
+sentMessage = None
+
 async def predictNickname(username, introduction):
 
     if os.path.exists('./nicknamePrompt.txt'):
@@ -28,7 +31,8 @@ async def predictNickname(username, introduction):
 
     prompt += f"\nName: {username}\nIntroduction: {introduction}\nNickname:"
 
-    print(prompt)
+    if len(prompt) > 2043:
+        prompt = prompt[0:2040]
 
     prediction = co.generate(
         model='large',
@@ -41,27 +45,8 @@ async def predictNickname(username, introduction):
     response = ' {}'.format(prediction.generations[0].text)[1:]
     return response
 
-@client.event
-async def on_ready():
-    print("logged in!")
-
-@client.event
-async def on_message(message):
-
-    # Check to see if we should even run code on it
-    if message.author == client.user:
-        return
-    elif message.channel.id != INTRO_CHANNEL:
-        if not (message.channel.id == OUTPUT_CHANNEL and message.content.startswith("-t")):
-            return
-
-    guild = client.get_guild(SERVER)
-    channel = guild.get_channel(OUTPUT_CHANNEL)
-
-    async with channel.typing():
-        username = message.author.name
-        avatar = message.author.avatar_url
-        introduction = message.content
+async def getNickname(username, introduction, avatar):
+    while (True):
         nickname = await predictNickname(username, introduction)
 
         # Clean up the response
@@ -69,9 +54,84 @@ async def on_message(message):
         if nickname[0] == " ":
             nickname = nickname[1:]
 
-        embed=discord.Embed(title=f"I hereby nickname them... {nickname}", color=0x21edff, description="Please let Libra know if this nickname is inappropriate!")
-        embed.set_author(name=f"{username} has posted an introduction!", icon_url=avatar)
+        if not (nickname == username or len(nickname.split(" ")) > 2):
+            print(f"Accepted nickname {nickname}")
+            break
+        else:
+            print(f"Rejected nickname {nickname}")
 
-    await channel.send(embed=embed)
+    embed=discord.Embed(title=f"I hereby nickname them... {nickname}", color=0x21edff, description="Please let Libra know if this nickname is inappropriate!")
+    embed.set_author(name=f"{username} has posted an introduction!", icon_url=avatar)
+    return embed
+
+@client.event
+async def on_ready():
+    print("logged in!")
+
+@client.event
+async def on_message(message):
+    global introMessage
+    global sentMessage
+
+    # Check to see if we should even run code on it
+    if message.author == client.user:
+        return
+    elif message.channel.id != INTRO_CHANNEL:
+        if not (message.channel.id == OUTPUT_CHANNEL and message.content.startswith("-t ")):
+            return
+
+    if sentMessage != None:
+        await sentMessage.clear_reactions()
+    introMessage = message
+
+    guild = client.get_guild(SERVER)
+    channel = guild.get_channel(OUTPUT_CHANNEL)
+
+    async with channel.typing():
+        username = message.author.name
+
+        introduction = message.content
+        if introduction.startswith("-t "):
+            introduction = introduction.split(" ")[1:]
+
+        avatar = message.author.avatar_url
+
+        embed = await getNickname(username, introduction, avatar)
+
+    sentMessage = await channel.send(embed=embed)
+    await sentMessage.add_reaction("🔄")
+
+@client.event
+async def on_raw_reaction_add(react_obj):
+    global introMessage
+    global sentMessage
+
+    if client.user == react_obj.member:
+        return
+    if not (react_obj.channel_id in [INTRO_CHANNEL, OUTPUT_CHANNEL]):
+        return
+
+    channel = client.get_channel(react_obj.channel_id)
+    message = await channel.fetch_message(react_obj.message_id)
+
+    if message == sentMessage:
+        if str(react_obj.emoji) == "🔄":
+            await sentMessage.clear_reactions()
+            embed=discord.Embed(title=f"Rerolling...", color=0x21edff)
+            await sentMessage.edit(embed=embed)
+
+            username = introMessage.author.name
+
+            introduction = introMessage.content
+            if introduction.startswith("-t "):
+                introduction = introduction.split(" ")[1:]
+
+            avatar = introMessage.author.avatar_url
+
+            embed = await getNickname(username, introduction, avatar)
+
+            await sentMessage.add_reaction("🔄")
+            await sentMessage.edit(embed=embed)
+
     
 client.run(DISCORD_KEY)
